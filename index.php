2d38b1f708daee7fde835422f6ec44df67470b2f
Enrico A <enrico.aurelius@gmail.com>
2021-11-25 13:21:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -295,7 +295,7 @@
         $dateSekarang = date("d/m/Y");
 
         $getData = $conn->query("SELECT * FROM tblRekap where tanggal=".$dateSekarang);
-        var_dump($getData == null);
+        var_dump($getData);
 
         $conn->query("INSERT INTO tblRekap(tanggal, jumlahmasuk, hargamasuk) VALUES ('".$dateSekarang."', ".$jmlBarang.",".$hargaBarang.")");
 
