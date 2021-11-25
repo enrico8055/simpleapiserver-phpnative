a392fa0d9f130761b59096b9dd8e8ce04e5c4e44
Enrico A <enrico.aurelius@gmail.com>
2021-11-25 11:01:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -263,7 +263,7 @@
             );
         }
     }else if(isset($_GET['action']) && $_GET['action'] == 14 && isset($_GET['id']) ){ //UNTUK AMBIL DATA REKAP DARI DB
-        $getTotal = $conn->query("SELECT (jumlahjual"."*"."hargajual)-ongkir as total FROM tblRinciRekap WHERE id=".$_GET['id']);
+        $getTotal = $conn->query("SELECT (jumlahjual*hargajual)-ongkir FROM tblRinciRekap WHERE id=".$_GET['id']);
         $getData = $conn->query("SELECT * FROM tblRinciRekap where id=".$_GET['id']); //ambil semua data dari db
         var_dump($getTotal);
         if($getTotal -> num_rows == 0 || $getData -> num_rows == 0){
