8d69be8c0d2a98b57ca04a163d4025e982e6504a
Enrico A <enrico.aurelius@gmail.com>
2021-11-25 11:07:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -263,7 +263,7 @@
             );
         }
     }else if(isset($_GET['action']) && $_GET['action'] == 14 && isset($_GET['id']) ){ //UNTUK AMBIL DATA REKAP DARI DB
-        $getTotal = $conn->query("SELECT (jumlahjual*hargajual)-ongkir FROM tblRinciRekap WHERE id=".$_GET['id']);
+        $getTotal = $conn->query("SELECT (jumlahjual*hargajual)-ongkir as total FROM tblRinciRekap WHERE id=".$_GET['id']);
         $getData = $conn->query("SELECT * FROM tblRinciRekap where id=".$_GET['id']); //ambil semua data dari db
         if($getTotal -> num_rows == 0 || $getData -> num_rows == 0){
             echo json_encode(
@@ -274,13 +274,12 @@
             
             while($row = $getData -> fetch_assoc()){ //kita ambil data per baris lalu masukkan ke tempat penampungan
                 $row2 = $getTotal -> fetch_assoc();
-                var_dump($row2);
                 array_push($result, array(
                     'namapembeli' => $row['namapembeli'],
                     'jumlahjual' => $row['jumlahjual'],
                     'hargajual' => $row['hargajual'],
                     'ongkir' => $row['ongkir'],
-                    'total' => $row['total']
+                    'total' => $row2['total']
                 ));
             };
 
